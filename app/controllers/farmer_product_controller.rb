10a505e62def522ac63c13c14a3e15c3a9c579a7
Nikolas Wünnenberg <n.wuennenberg@gmail.com>
2021-03-03 11:55:01 +0100

    famer selects farmer products

---
+++ app/controllers/farmer_product_controller.rb
@@ -0,0 +1,25 @@
+class FarmerProductsController < ApplicationController
+
+  def new
+    @product = Product.find(params[:product_id])
+    @farmer_product = FarmerProduct.new
+  end
+
+  def create
+    @products = Product.all
+    @farmer_product = FarmerProduct.new(farmer_product_params)
+    @farmer_product.user = current_user
+    if @farmer_product.save
+      #change redirect to index
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def farmer_product_params
+    params.require(:farmer_product).permit(:product_id)
+  end
+end
