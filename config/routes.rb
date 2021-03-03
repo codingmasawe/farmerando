029881df4bd4fa63ff439865c878f47fb4f3cd50
Nikolas Wünnenberg <n.wuennenberg@gmail.com>
2021-03-03 17:20:01 +0100

    propose transaction

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.
-  resources :products do
     resources :farmer_products, only: [:new, :show, :create]
-  end
+    get "profile", to: "users#profile"
+
+  resources :transaction, only: [:new, :show, :create]
 end
