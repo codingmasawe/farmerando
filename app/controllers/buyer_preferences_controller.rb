ec82bb62535cd9442fc856cdf2bfcb212aade7e3
samya cardoso de david <dedavidsamya@gmail.com>
2021-03-03 16:00:09 +0100

    user controller etc

--- app/controllers/buyer_preferences_controller.rb
+++ app/controllers/buyer_preferences_controller.rb
@@ -1,2 +1,17 @@
 class BuyerPreferencesController < ApplicationController
+  def new
+    @buyer_preference = BuyerPreference.new
+  end
+
+  def create
+    @buyer_preference = BuyerPreference.new(buyer_preference_params)
+    @buyer_preference.user = current_user
+    @buyer_preference.product = Product.find(buyer_preference_params[:product_id])
+  end
+
+  private
+
+  def buyer_preference_params
+    params.require(:buyer_preference).permit(:price, :user, :product)
+  end
 end
