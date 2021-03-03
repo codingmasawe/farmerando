7aba3bf05f45fe7c43e1903fb709837023ae84ba
samya cardoso de david <dedavidsamya@gmail.com>
2021-03-03 14:23:22 +0100

    dashboard controller created, there is no view yet

--- app/controllers/buyer_preferences_controller.rb
+++ app/controllers/buyer_preferences_controller.rb
@@ -1,2 +1,3 @@
 class BuyerPreferencesController < ApplicationController
+
 end
---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,5 @@
+class DashboardController < ApplicationController
+   def index
+    @products = ["banana", "tomato", "potato"]
+  end
+end
