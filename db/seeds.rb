029338d25967880ba5a842d5aabbd149c29d84df
Nikolas Wünnenberg <n.wuennenberg@gmail.com>
2021-03-11 17:33:29 +0100

    new_seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,7 +30,7 @@ godin = User.create!(email: "godin@godin.com", password: "123456", first_name: "
 innocent = User.create!(email: "innocent@innocent.com", password: "123456", first_name: "Innocent", last_name: "Obiri", location: "Kampala", img_url: "users/innocent.jpg", role: "buyer")
 
 jessica = User.create!(email: "jessica@jessica.com", password: "123456", first_name: "Jessica", last_name: "Ariwa", location: "Kampala", img_url: "users/jessica.jpg", role: "farmer")
-barack = User.create!(email: "frank@frank.com", password: "123456", first_name: "Frank", last_name: "Mumba", location: "Kampala", img_url: "users/frank.jpg", role: "farmer")
+frank = User.create!(email: "frank@frank.com", password: "123456", first_name: "Frank", last_name: "Mumba", location: "Kampala", img_url: "users/frank.jpg", role: "farmer")
 
 # u10 = User.create!(email: "richard@richard.com", password: "123456", first_name: "Richard", last_name: "Mollo", location: "Libreville", img_url: "/app/assets/images/users/richard.jpg")
 # u11 = User.create!(email: "cori@cori.com", password: "123456", first_name: "Cori", last_name: "Cando", location: "Libreville", img_url: "/app/assets/images/users/cori.jpg")
